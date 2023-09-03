5a8c319acf952aa00056830038c54b11a966a8f5
kevin5806 <kevinleoni58@gmail.com>
2023-09-03 21:30:51 +0200

    Update ddos.js

--- netlify/functions/ddos.js
+++ netlify/functions/ddos.js
@@ -1,17 +1,20 @@
 const http = require('http');
 
 for (let i = 1; i < 30; i++) {
-
+    
     const target = {
-        hostname: `ddos.kevinleoni.me/.netlify/functions/ddos${i}`,
+        hostname: 'ddos.kevinleoni.me/.netlify/functions/ddos${i}',
         port: 80,
         path: '/',
         method: 'GET'
-      }
+    }
 
     const req = http.request(target);
+
+    req.on('response', (response) => { });
+    req.on('error', (error) => { });
+
     req.end();
-    
 }
 
 exports.handler = async (event, context) => {
