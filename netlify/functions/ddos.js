c2014dc506ce391ce30025a8afb9b38823666db0
kevin5806 <kevinleoni58@gmail.com>
2023-09-03 20:58:15 +0200

    Update ddos.js

--- netlify/functions/ddos.js
+++ netlify/functions/ddos.js
@@ -3,7 +3,7 @@ const http = require('http');
 for (let i = 0; i < 30; i++) {
 
     const target = {
-        hostname: `https://funny-vacherin-bf6e08.netlify.app/.netlify/functions/ddos%copy%${i}`,
+        hostname: `https://funny-vacherin-bf6e08.netlify.app/.netlify/functions/ddos${i}`,
         port: 80,
         path: '/',
         method: 'GET'
